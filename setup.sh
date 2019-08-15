8eb5694fbbe781e3aafc9f25ee7ef3a8775de039
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2019-08-15 11:16:33 -0400

    Minor fixes, remove unused module

--- setup.sh
+++ setup.sh
@@ -9,12 +9,11 @@ cd portaudio/
 
 ./configure
 sudo make install
-
 cd ..
 
 sudo apt update
 sudo apt install libatlas-base-dev espeak pulseaudio
-python2 -m pip install python-vlc pyttsx3 numpy
+python2 -m pip install python-vlc pyttsx3
 
 echo "2/4 Opening Firefox to http://docs.kitt.ai/snowboy/"
 echo "Please download the correct version of Snowboy for your system and place it in ~/Sonica"
@@ -25,10 +24,16 @@ read
 
 echo "3/4 Extracting Snowboy"
 echo "Expect some errors, these can be ignored safely"
+
 tar -xf ubuntu1404-x86_64-1.1.1.tar.bz2
 tar -xf rpi-arm-raspbian-8.0-1.1.1.tar.bz2
+tar -xf pine64-debian-jessie-1.1.1.tar.bz2
+tar -xf edison-ubilinux-1.1.1.tar.bz2
+
 mv ubuntu1404-x86_64-1.1.1 snowboy
 mv rpi-arm-raspbian-8.0-1.1.1 snowboy
+mv pine64-debian-jessie-1.1.1 snowboy
+mv edison-ubilinux-1.1.1 snowboy
 
 echo "4/4 Downloading sonica.py"
 wget https://github.com/sciboy12/Sonica/raw/master/sonica.py
